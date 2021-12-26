3b5d22eceebe33d61fce8df93a9f38b38a656354
Alena Bauer <alena.a.bauer@gmail.com>
2021-12-26 11:11:19 +0100

    Customize modals

--- script.js
+++ script.js
@@ -5,14 +5,18 @@ const btnsCloseModal = document.querySelectorAll('.close-modal');
 const overlay = document.querySelector('.overlay');
 const btnsShowModal = document.querySelectorAll('.show-modal');
 
+const addCloseFunction = (target, modal) => {
+  target.addEventListener('click', (e) => {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  })
+}
+
 for (let i = 0; i < btnsShowModal.length; i++) {
   btnsShowModal[i].addEventListener('click', (e) => {
-    console.log(`${e.currentTarget.textContent} clicked!`)
     modals[i].classList.remove('hidden');
     overlay.classList.remove('hidden');
   })
-  btnsCloseModal[i].addEventListener('click', (e) => {
-    modals[i].classList.add('hidden');
-    overlay.classList.add('hidden');
-  })
+  addCloseFunction(btnsCloseModal[i], modals[i]);
+  addCloseFunction(overlay, modals[i]); // close the modal by clicking on anything outside it
 }
